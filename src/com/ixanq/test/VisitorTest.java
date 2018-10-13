32495c156a3fdea543d2d6fdde7a0c92903f3023
ixanq <3236734502@qq.com>
2018-10-13 16:44:20 +0800

    人力资源管理系统

--- src/com/ixanq/test/VisitorTest.java
+++ src/com/ixanq/test/VisitorTest.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:applicationContext.xml")
-public class HRMTest {
+public class VisitorTest {
 
     @Autowired
     private VisitorService visitorService;
